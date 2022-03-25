32351a9d6851a5207d95a98638f766d03012a316
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-03-25 23:58:47 +0600

    Main component created

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div>
+      <div className=" main-section">
+        <div className="main-left">Left</div>
+        <div className="main-right">Right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
